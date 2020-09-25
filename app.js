367f35aaa61582097c3703dcfff3ff00eb236d50
X500658 <clydevincentpilapil@gmail.com>
2020-09-26 02:58:42 +0800

    Login and Sign Up
    tweaks:
    
    fixing index.ejs >span
    adding title variable to header.ejs
    adding proper links to navbar.ejs
    adding routes to app.js

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 
+const site=" | Red Cross Youth Chapter";
+
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req,res)=>{
-    res.render('index');
+    res.render('index', {title: "Home" + site});
+});
+
+app.get('/login', (req,res)=>{
+    res.render('login', {title: "Login" + site});
+});
+
+app.get('/signup', (req,res)=>{
+    res.render('signup', {title: "Sign Up" + site});
 });
 
 app.get('/about', (req,res)=>{
-    res.render('about');
+    res.render('about', {title: "About" + site});
 });
 
 app.listen(3000);
