27b6d3bdb37451db3270a82548d710d48746db18
chickenpew <58925125+chickenpew@users.noreply.github.com>
2020-09-27 11:59:57 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 
+
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req,res)=>{
-    res.render('index');
+    res.render('login');
+});
+app.get('/home', (req,res)=>{
+    res.render('home');
 });
 
-app.get('/about', (req,res)=>{
-    res.render('about');
+app.get('/officerActivity', (req,res) =>{
+    res.render('officerActivity');
+});
+
+app.get('/adminActivity', (req,res) =>{
+    res.render('adminActivity');
+});
+
+app.get('/activityForm', (req,res)=>{
+    res.render('addReport');
 });
 
 app.listen(3000);
