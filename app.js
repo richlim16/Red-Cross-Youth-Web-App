25ed3e41710c4a44762b847cf32fdc9c68de83c5
X500658 <clydevincentpilapil@gmail.com>
2020-09-27 12:20:22 +0800

    Members page added
    made by reusing derek's old php code
    
    my pc view is 4 cards are most
    my phone view is 1 at most
    
    app.js has dummy arrays for viewing, don't judge my waifus :(
    
    idk with the card implementation if scalable to be smaller for phones :)

--- app.js
+++ app.js
@@ -12,6 +12,28 @@ app.get('/', (req,res)=>{
     res.render('index', {title: "Home" + site});
 });
 
+app.get('/members', (req,res)=>{
+    ads=[
+        {name:"Nakano Miku", pic: "http://imgur.com/hRst2bwh.jpg", id:1},
+        {name: "Rory Mercury", pic: "http://imgur.com/GaBdGdz.png", id:2}
+    ];
+    ads2=[
+        {name:"Nakano Miku", pic: "http://imgur.com/hRst2bwh.jpg", id:1},
+        {name: "Rory Mercury", pic: "http://imgur.com/GaBdGdz.png", id:2},
+        {name: "Dlanor A. Knox", pic: "https://i.imgur.com/ztUVQBU.jpg", id:3},
+        {name: "Kiryuu Minazuki", pic: "http://imgur.com/TiHTR8N.jpg", id:4},
+        {name: "Kushina Anna", pic: "http://imgur.com/5Pz32yG.jpg", id:5}
+    ];
+    // console.log(ads);
+    res.render('members', {
+        title: "Members" + site,
+        sysAds: ads,
+        appAds: ads,
+        couAdv: ads2,
+        couOff: ads2
+    });
+});
+
 app.get('/login', (req,res)=>{
     res.render('login', {title: "Login" + site});
 });
