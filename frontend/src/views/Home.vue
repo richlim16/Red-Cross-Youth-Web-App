9168211c2f0638293fc9773f5af48764e04756aa
Shinji Ishikawa <shinG.ishikawa@gmail.com>
2020-12-10 23:24:12 +0800

    added router and homepage

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,668 @@
+<template>
+    <div id="homepage">
+        <section id="hero" class="d-flex align-items-center">
+        <div class="container" data-aos="zoom-out" data-aos-delay="100">
+        <h1>
+            <p>Welcome to the</p>
+            <span>Red Cross Youth Document Management System!</span>
+        </h1>
+        <h2>Siamo Tutti Fratelli!</h2>
+        <div class="d-flex">
+            <a href="#about" class="btn-get-started scrollto">Get Started</a>
+            <a href="https://www.youtube.com/watch?v=HwpzzAefx9M" class="venobox btn-watch-video" data-vbtype="video" data-autoplay="true"> Watch Video <i class="icofont-play-alt-2"></i></a>
+        </div>
+        </div>
+    </section><!-- End Hero --> 
+    <!-- ======= About Section ======= -->
+      <section id="about" class="about section-bg">
+        <div class="container" data-aos="fade-up">
+  
+          <div class="section-title">
+            <h2>About</h2>
+            <h3>The <span>Red Cross Youth</span></h3>
+            <p>As a service</p>
+          </div>
+  
+          <div class="row">
+            <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
+              <img src="https://www.sunstar.com.ph/uploads/images/2019/06/16/153516.jpg" class="img-fluid" alt="Image failed to load">
+            </div>
+            <div class="col-lg-6 pt-4 pt-lg-0 content d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100">
+              <h3>The Red Cross Youth</h3>
+              <p class="font-italic">
+                  The Red Cross Youth is one of six major services provided by the Philippine Red Cross. The Red Cross Youth Promotes volunteerism and inspires the youth through voluntary service and aleviating human suffering!
+              </p>
+              <ul>
+                <li>
+                  <!--<i class="bx bx-store-alt"></i>-->
+                  <div>                  
+                    <p class="font-italic">7 Fundamental Principles of the Red Cross and Red Crescent Movement</p>
+                    <h5>𝘏𝘶𝘮𝘢𝘯𝘪𝘵𝘺</h5>
+                    <p>𝘞𝘦 𝘴𝘦𝘳𝘷𝘦 𝘱𝘦𝘰𝘱𝘭𝘦 𝘣𝘶𝘵 𝘯𝘰𝘵 𝘴𝘺𝘴𝘵𝘦𝘮𝘴.</p>
+                  </div>
+                </li>
+              </ul><!--leaving it like as a template lang we can pageify or make it scrollable -->
+              <p class="font-italic">
+                𝘈𝘴 𝘢 𝘺𝘰𝘶𝘵𝘩 𝘮𝘦𝘮𝘣𝘦𝘳 𝘰𝘧 𝘵𝘩𝘦 𝘗𝘳𝘦𝘮𝘪𝘦𝘳𝘦 𝘏𝘶𝘮𝘢𝘯𝘪𝘵𝘢𝘳𝘪𝘢𝘯 𝘖𝘳𝘨𝘢𝘯𝘪𝘻𝘢𝘵𝘪𝘰𝘯; 𝘐 𝘱𝘭𝘦𝘥𝘨𝘦 𝘵𝘰 𝘢𝘭𝘭𝘦𝘷𝘪𝘢𝘵𝘦 𝘩𝘶𝘮𝘢𝘯 𝘴𝘶𝘧𝘧𝘦𝘳𝘪𝘯𝘨; 𝘵𝘰 𝘱𝘳𝘰𝘵𝘦𝘤𝘵 𝘭𝘪𝘧𝘦 𝘢𝘯𝘥 𝘥𝘪𝘨𝘯𝘪𝘵𝘺; 𝘵𝘰 𝘣𝘦 𝘢 𝘳𝘦𝘴𝘱𝘰𝘯𝘴𝘪𝘣𝘭𝘦 𝘤𝘪𝘵𝘪𝘻𝘦𝘯 𝘰𝘧 𝘵𝘩𝘦 𝘙𝘦𝘱𝘶𝘣𝘭𝘪𝘤 𝘰𝘧 𝘵𝘩𝘦 𝘗𝘩𝘪𝘭𝘪𝘱𝘱𝘪𝘯𝘦𝘴, 𝘵𝘰 𝘤𝘰𝘯𝘧𝘰𝘳𝘮 𝘸𝘪𝘵𝘩 𝘵𝘩𝘦 𝘰𝘣𝘫𝘦𝘤𝘵𝘪𝘷𝘦𝘴 𝘢𝘯𝘥 𝘱𝘰𝘭𝘪𝘤𝘪𝘦𝘴 𝘰𝘧 𝘵𝘩𝘦 𝘗𝘩𝘪𝘭𝘪𝘱𝘱𝘪𝘯𝘦 𝘙𝘦𝘥 𝘊𝘳𝘰𝘴𝘴; 𝘪𝘵𝘴 𝘴𝘦𝘷𝘦𝘯 𝘧𝘶𝘯𝘥𝘢𝘮𝘦𝘯𝘵𝘢𝘭 𝘱𝘳𝘪𝘯𝘤𝘪𝘱𝘭𝘦𝘴 𝘏𝘶𝘮𝘢𝘯𝘪𝘵𝘺, 𝘐𝘮𝘱𝘢𝘳𝘵𝘪𝘢𝘭𝘪𝘵𝘺, 𝘕𝘦𝘶𝘵𝘳𝘢𝘭𝘪𝘵𝘺, 𝘐𝘯𝘥𝘦𝘱𝘦𝘯𝘥𝘦𝘯𝘤𝘦, 𝘝𝘰𝘭𝘶𝘯𝘵𝘢𝘳𝘺 𝘚𝘦𝘳𝘷𝘪𝘤𝘦, 𝘜𝘯𝘪𝘵𝘺, 𝘜𝘯𝘪𝘷𝘦𝘳𝘴𝘢𝘭𝘪𝘵𝘺 𝘢𝘯𝘥 𝘵𝘰 𝘣𝘦 𝘢𝘵 𝘢𝘭𝘭 𝘵𝘪𝘮𝘦𝘴 𝘣𝘦 𝘪𝘯 𝘴𝘦𝘳𝘷𝘪𝘤𝘦 𝘧𝘰𝘳 𝘵𝘩𝘦 𝘴𝘶𝘧𝘧𝘦𝘳𝘪𝘯𝘨 𝘩𝘶𝘮𝘢𝘯𝘪𝘵𝘺.
+              </p>
+            </div>
+          </div>
+  
+        </div>
+      </section><!-- End About Section -->
+      <!-- ======= Team Section ======= -->
+    <section id="team" class="team section-bg">
+        <div class="container" data-aos="fade-up">  
+          <div class="section-title">
+            <h2>Chapter Youth Council</h2>
+            <h3>Our Hardworking <span>Team</span></h3>
+            <p class="font-italic">"To give and to not to count the cost..."</p>
+            <p class="font-italic">-Sto.Ignacio de Loyola</p>
+          </div>
+          <div class="row">
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+              <div class="member">
+                <div class="member-img">
+                  <img src="/images/jojoSiwa.jpg" class="img-fluid" alt="">
+                  <div class="social">
+                    <a href=""><i class="icofont-twitter"></i></a>
+                    <a href=""><i class="icofont-facebook"></i></a>
+                    <a href=""><i class="icofont-instagram"></i></a>
+                    <a href=""><i class="icofont-linkedin"></i></a>
+                  </div>
+                </div>
+                <div class="member-info">
+                  <h4>Jojo Siwa</h4>
+                  <span>Chief Executive Officer</span>
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+              <div class="member">
+                <div class="member-img">
+                  <img src="/images/karen.jpg" class="img-fluid" alt="">
+                  <div class="social">
+                    <a href=""><i class="icofont-twitter"></i></a>
+                    <a href=""><i class="icofont-facebook"></i></a>
+                    <a href=""><i class="icofont-instagram"></i></a>
+                    <a href=""><i class="icofont-linkedin"></i></a>
+                  </div>
+                </div>
+                <div class="member-info">
+                  <h4>Karen</h4>
+                  <span>Useless Person</span>
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+              <div class="member">
+                <div class="member-img">
+                  <img src="/images/lilPichu.png" class="img-fluid" alt="">
+                  <div class="social">
+                    <a href=""><i class="icofont-twitter"></i></a>
+                    <a href=""><i class="icofont-facebook"></i></a>
+                    <a href=""><i class="icofont-instagram"></i></a>
+                    <a href=""><i class="icofont-linkedin"></i></a>
+                  </div>
+                </div>
+                <div class="member-info">
+                  <h4>LilyPichu</h4>
+                  <span>CTO</span>
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
+              <div class="member">
+                <div class="member-img">
+                  <img src="https://pbs.twimg.com/media/EfM-mcPVAAMuAGs.jpg" class="img-fluid" alt="">
+                  <div class="social">
+                    <a href=""><i class="icofont-twitter"></i></a>
+                    <a href=""><i class="icofont-facebook"></i></a>
+                    <a href=""><i class="icofont-instagram"></i></a>
+                    <a href=""><i class="icofont-linkedin"></i></a>
+                  </div>
+                </div>
+                <div class="member-info">
+                  <h4>Michael Reeves</h4>
+                  <span>Accountant</span>
+                </div>
+              </div>
+            </div><!-- let's consider using these cards instead of the ones i used sa prototype, this looks much cleaner than mine -derek-->          
+          </div>  
+        </div>
+      </section><!-- End Team Section -->
+      <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+        <div class="container" data-aos="fade-up">
+  
+          <div class="section-title">
+            <h2>Contact</h2>
+            <h3><span>Contact Us</span></h3>
+            <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
+          </div>
+  
+          <div class="row" data-aos="fade-up" data-aos-delay="100">
+            <div class="col-lg-6">
+              <div class="info-box mb-4">
+                <i class="bx bx-map"></i>
+                <h3>Our Address</h3>
+                <p>A108 Adam Street, New York, NY 535022</p>
+              </div>
+            </div>
+  
+            <div class="col-lg-3 col-md-6">
+              <div class="info-box  mb-4">
+                <i class="bx bx-envelope"></i>
+                <h3>Email Us</h3>
+                <p>ceburcy@gmail.com</p>
+              </div>
+            </div>
+  
+            <div class="col-lg-3 col-md-6">
+              <div class="info-box  mb-4">
+                <i class="bx bx-phone-call"></i>
+                <h3>Call Us</h3>
+                <p>+1 5589 55488 55</p>
+              </div>
+            </div>
+  
+          </div>
+  
+          <div class="row" data-aos="fade-up" data-aos-delay="100">
+  
+            <div class="col-lg-6 ">
+              <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
+            </div>
+  
+            <div class="col-lg-6">
+              <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                <div class="row">
+                  <div class="col form-group">
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                    <div class="validate"></div>
+                  </div>
+                  <div class="col form-group">
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                    <div class="validate"></div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                  <div class="validate"></div>
+                </div>
+                <div class="form-group">
+                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                  <div class="validate"></div>
+                </div>
+                <div class="mb-3">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+                </div>
+                <div class="text-center"><button type="submit">Send Message</button></div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section><!-- End Contact Section -->  
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home'
+}
+</script>
+
+<style scoped>
+    /*--------------------------------------------------------------
+    # Hero Section
+    --------------------------------------------------------------*/
+    #hero {
+    width: 100%;
+    height: 75vh;
+    background: url("/images/REDCROSS123-1280x720.jpg") top left;
+    background-size: cover;
+    position: relative;
+    }
+
+    #hero:before {
+    content: "";
+    background: rgba(255, 255, 255, 0.6);
+    position: absolute;
+    bottom: 0;
+    top: 0;
+    left: 0;
+    right: 0;
+    }
+
+    #hero .container {
+    position: relative;
+    padding-top: 132px;
+    }
+
+    @media (max-width: 992px) {
+    #hero .container {
+        padding-top: 58px;
+    }
+    }
+
+    #hero h1 {
+    margin: 0;
+    font-size: 48px;
+    font-weight: 700;
+    line-height: 56px;
+    color: #222222;
+    font-family: "Poppins", sans-serif;
+    }
+
+    #hero h1 span {
+    color: #ea1010;
+    }
+
+    #hero h2 {
+    color: #555555;
+    margin: 5px 0 30px 0;
+    font-size: 24px;
+    font-weight: 400;
+    }
+
+    #hero .btn-get-started {
+    font-family: "Roboto", sans-serif;
+    text-transform: uppercase;
+    font-weight: 500;
+    font-size: 14px;
+    letter-spacing: 1px;
+    display: inline-block;
+    padding: 10px 28px;
+    border-radius: 4px;
+    transition: 0.5s;
+    color: #fff;
+    background: #ea1010;
+    }
+
+    #hero .btn-get-started:hover {
+    background: #e65b5b;
+    }
+
+    #hero .btn-watch-video {
+    font-size: 16px;
+    display: inline-block;
+    padding: 10px 25px 8px 40px;
+    transition: 0.5s;
+    margin-left: 25px;
+    color: #222222;
+    position: relative;
+    font-weight: 600;
+    }
+
+    #hero .btn-watch-video i {
+    color: #ea1010;
+    font-size: 32px;
+    position: absolute;
+    left: 0;
+    top: 7px;
+    transition: 0.3s;
+    }
+
+    #hero .btn-watch-video:hover {
+    color: #ea1010;
+    }
+
+    #hero .btn-watch-video:hover i {
+    color: #eb5f5f;
+    }
+
+    @media (min-width: 1024px) {
+    #hero {
+        background-attachment: fixed;
+    }
+    }
+
+    @media (max-width: 768px) {
+        #hero {
+            height: 100vh;
+        }
+        #hero h1 {
+            font-size: 28px;
+            line-height: 36px;
+        }
+        #hero h2 {
+            font-size: 18px;
+            line-height: 24px;
+            margin-bottom: 30px;
+        }
+        #hero .btn-get-started, #hero .btn-watch-video {
+            font-size: 13px;
+        }
+    }
+
+    @media (max-height: 500px) {
+        #hero {
+            height: 120vh;
+        }
+    }
+
+    /*--------------------------------------------------------------
+    # Sections General
+    --------------------------------------------------------------*/
+    section {
+    padding: 60px 0;
+    overflow: hidden;
+    }
+
+    .section-bg {
+    background-color: #f6f9fe;
+    }
+
+    .section-title {
+    text-align: center;
+    padding-bottom: 30px;
+    }
+
+    .section-title h2 {
+    font-size: 13px;
+    letter-spacing: 1px;
+    font-weight: 700;
+    padding: 8px 20px;
+    margin: 0;
+    background: #e7f1fd;
+    color: #ea1010;
+    display: inline-block;
+    text-transform: uppercase;
+    border-radius: 50px;
+    }
+
+    .section-title h3 {
+    margin: 15px 0 0 0;
+    font-size: 32px;
+    font-weight: 700;
+    }
+
+    .section-title h3 span {
+    color: #ea1010;
+    }
+
+    .section-title p {
+    margin: 15px auto 0 auto;
+    font-weight: 600;
+    }
+
+    @media (min-width: 1024px) {
+    .section-title p {
+        width: 50%;
+    }
+    }
+
+    /*--------------------------------------------------------------
+    # About
+    --------------------------------------------------------------*/
+    .about .content h3 {
+    font-weight: 600;
+    font-size: 26px;
+    }
+
+    .about .content ul {
+    list-style: none;
+    padding: 0;
+    }
+
+    .about .content ul li {
+    display: flex;
+    align-items: flex-start;
+    margin-bottom: 35px;
+    }
+
+    .about .content ul li:first-child {
+    margin-top: 35px;
+    }
+
+    .about .content ul i {
+    background: #fff;
+    box-shadow: 0px 6px 15px rgba(16, 110, 234, 0.12);
+    font-size: 24px;
+    padding: 20px;
+    margin-right: 15px;
+    color: #ea1010;
+    border-radius: 50px;
+    }
+
+    .about .content ul h5 {
+    font-size: 18px;
+    color: #555555;
+    }
+
+    .about .content ul p {
+    font-size: 15px;
+    }
+
+    .about .content p:last-child {
+    margin-bottom: 0;
+    }
+
+    /*--------------------------------------------------------------
+    # Team
+    --------------------------------------------------------------*/
+    .team {
+    padding: 60px 0;
+    background: rgba(255, 255, 255, 0.568);
+    }
+
+    .team .member {
+    margin-bottom: 20px;
+    overflow: hidden;
+    border-radius: 4px;
+    background: #fff;
+    box-shadow: 0px 2px 15px rgba(16, 110, 234, 0.15);
+    }
+
+    .team .member .member-img {
+    position: relative;
+    overflow: hidden;
+    }
+
+    .team .member .social {
+    position: absolute;
+    left: 0;
+    bottom: 30px;
+    right: 0;
+    opacity: 0;
+    transition: ease-in-out 0.3s;
+    text-align: center;
+    }
+
+    .team .member .social a {
+    transition: color 0.3s;
+    color: #222222;
+    margin: 0 3px;
+    padding-top: 7px;
+    border-radius: 4px;
+    width: 36px;
+    height: 36px;
+    background: rgba(234, 16, 16, 0.8);
+    display: inline-block;
+    transition: ease-in-out 0.3s;
+    color: #fff;
+    }
+
+    .team .member .social a:hover {
+    background: #f23b3b;
+    }
+
+    .team .member .social i {
+    font-size: 18px;
+    }
+
+    .team .member .member-info {
+    padding: 25px 15px;
+    }
+
+    .team .member .member-info h4 {
+    font-weight: 700;
+    margin-bottom: 5px;
+    font-size: 18px;
+    color: #222222;
+    }
+
+    .team .member .member-info span {
+    display: block;
+    font-size: 13px;
+    font-weight: 400;
+    color: #aaaaaa;
+    }
+
+    .team .member .member-info p {
+    font-style: italic;
+    font-size: 14px;
+    line-height: 26px;
+    color: #777777;
+    }
+
+    .team .member:hover .social {
+    opacity: 1;
+    bottom: 15px;
+    }
+
+    /*--------------------------------------------------------------
+    # Contact
+    --------------------------------------------------------------*/
+    .contact .info-box {
+    color: #444444;
+    text-align: center;
+    box-shadow: 0 0 30px rgba(214, 215, 216, 0.3);
+    padding: 20px 0 30px 0;
+    }
+
+    .contact .info-box i {
+    font-size: 32px;
+    color: #ea1010;
+    border-radius: 50%;
+    padding: 8px;
+    border: 2px dotted #b3d1fa;
+    }
+
+    .contact .info-box h3 {
+    font-size: 20px;
+    color: #777777;
+    font-weight: 700;
+    margin: 10px 0;
+    }
+
+    .contact .info-box p {
+    padding: 0;
+    line-height: 24px;
+    font-size: 14px;
+    margin-bottom: 0;
+    }
+
+    .contact .php-email-form {
+    box-shadow: 0 0 30px rgba(214, 215, 216, 0.4);
+    padding: 30px;
+    }
+
+    .contact .php-email-form .validate {
+    display: none;
+    color: red;
+    margin: 0 0 15px 0;
+    font-weight: 400;
+    font-size: 13px;
+    }
+
+    .contact .php-email-form .error-message {
+    display: none;
+    color: #fff;
+    background: #ed3c0d;
+    text-align: left;
+    padding: 15px;
+    font-weight: 600;
+    }
+
+    .contact .php-email-form .error-message br + br {
+    margin-top: 25px;
+    }
+
+    .contact .php-email-form .sent-message {
+    display: none;
+    color: #fff;
+    background: #18d26e;
+    text-align: center;
+    padding: 15px;
+    font-weight: 600;
+    }
+
+    .contact .php-email-form .loading {
+    display: none;
+    background: #fff;
+    text-align: center;
+    padding: 15px;
+    }
+
+    .contact .php-email-form .loading:before {
+    content: "";
+    display: inline-block;
+    border-radius: 50%;
+    width: 24px;
+    height: 24px;
+    margin: 0 10px -6px 0;
+    border: 3px solid #18d26e;
+    border-top-color: #eee;
+    -webkit-animation: animate-loading 1s linear infinite;
+    animation: animate-loading 1s linear infinite;
+    }
+
+    .contact .php-email-form .form-group {
+    margin-bottom: 20px;
+    }
+
+    .contact .php-email-form input, .contact .php-email-form textarea {
+    border-radius: 0;
+    box-shadow: none;
+    font-size: 14px;
+    }
+
+    .contact .php-email-form input:focus, .contact .php-email-form textarea:focus {
+    border-color: #ea1010;
+    }
+
+    .contact .php-email-form input {
+    padding: 10px 15px;
+    }
+
+    .contact .php-email-form textarea {
+    padding: 12px 15px;
+    }
+
+    .contact .php-email-form button[type="submit"] {
+    background: #ea1010;
+    border: 0;
+    padding: 10px 30px;
+    color: #fff;
+    transition: 0.4s;
+    border-radius: 4px;
+    }
+
+    .contact .php-email-form button[type="submit"]:hover {
+    background: #3b8af2;
+    }
+
+    @-webkit-keyframes animate-loading {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+    }
+
+    @keyframes animate-loading {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+    }
+</style>
