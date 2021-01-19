2bba126b76c62f79f7100b7cb55f9a323fda88fc
Derek-Buttered <derekyu565@gmail.com>
2021-01-19 19:54:14 +0800

    Let's try this again

--- dbconnection.js
+++ dbconnection.js
@@ -1,5 +1,5 @@
 const {Sequelize} = require('sequelize');
-const sequelize = new Sequelize('rcy_db', 'root', '', {
+const sequelize = new Sequelize('fh760xjq4bv7hcgj', 'm6kvxzjvw4fvofsl', 'exxzpyih9awp6pa1', {
     host: 'lfmerukkeiac5y5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     pool: {
@@ -17,3 +17,10 @@ try{
 }
 
 exports.sequelize = sequelize;
+
+/*
+    host:"lfmerukkeiac5y5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user:"m6kvxzjvw4fvofsl",
+    password:"exxzpyih9awp6pa1",
+    database:"fh760xjq4bv7hcgj"
+*/
